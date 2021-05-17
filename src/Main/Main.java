87223bd6e59b93c5cc2779701f1fe6ba636caadb
Vaibhav Kapoor <60018343+vaibhavkpr@users.noreply.github.com>
2021-05-16 18:50:42 -0600

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,11 +7,7 @@ import GameMedia.Media;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		//Reset Background
-		//background(background);
-		
+		// TODO Auto-generated method stub		
 		JFrame window = new JFrame("Lunar Lander");
 		window.setContentPane(new GamePanel());
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
